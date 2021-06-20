417eed15b456387711748d1be5f7e280cd1a5e25
Lucy Lewitt <llewitt@outlook.com>
2021-06-20 11:19:43 +0100

    Probably going to move back a few commits and start over.

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
-from sys import exit
+from sys import exit, argv
 from os import mkdir
 from os.path import basename, getsize, join
-from pdb import set_trace
+from struct import pack
 
 def pad_string(string, length):
     return ((length - len(string)) * " ") + string
+
+def pad_bytes(buf, word_size):
+    if (len(buf) % word_size) != 0:
+        if type(buf) == str:
+            buf += str((0).to_bytes(word_size - (len(buf) % word_size), "little"))
+
+        else:
+            buf += (0).to_bytes(word_size - (len(buf) % word_size), "little")
+
+    return buf
+
+def pad_size(size, word_size):
+    if (size % word_size) != 0:
+        size += word_size - (size % word_size)
+
+    return size
 
 class File:
     def __init__(self, path, read = True):
@@ -17,6 +33,7 @@ class File:
 
         if self.read:
             self.size = getsize(self.path)
+        
         else:
             self.size = 0
             
@@ -26,78 +43,123 @@ class File:
 class Toc(File):
     def __init__(self, path, read = True):
         super().__init__(path, read)
-        self.entries = []
 
-        with open(self.path, "r") as handle:
-            for line in handle.readlines()[1:]:
-                self.entries.append(TocEntry(line))
-        
+        if self.read:
+            self.entries = []
+
+            with open(self.path, "r") as handle:
+                for line in handle.readlines()[1:]:
+                    self.entries.append(TocEntry(line))
+        else:
+            with open(self.path, "w") as handle:
+                handle.write("UT\n")
+
+    def add(self, entry):
+        if type(entry) == str:
+            size = pad_size(getsize(entry), 4)
+            name = basename(entry)
+            name_size = len(name)
+
+        if type(entry) == Zib:
+            size = entry.size
+            name = basename(entry.name)
+            name_size = len(name)
+
+        with open(self.path, "a") as handle:
+            handle.write(f"\t{hex(size)[2:]}\t{hex(name_size)[2:]}\t{name}\n")
+
 class TocEntry:
     def __init__(self, line):
         size, name_size, self.name = line.split()
-        self.size = int(size, base = 16)
+        self.size = pad_size(int(size, base = 16), 4)
         self.name_size = int(name_size, base = 16)
 
 class Zib:
-    def __init__(self, buf, name):
-        self.name = name
-        self.entries = []
+    def __init__(self, *args, read = True):
+        self.read = read
+        self.entry_size = 64
+        if self.read:
+            buf = args[0]
+            self.name = args[1]
+            self.entries = []
 
-        if ".jpg" in self.name:
-            field_size = 8
-            address_zero_correction = 0
-        else:
-            field_size = 4
-            address_zero_correction = -1
+            if ".jpg" in self.name:
+                field_size = 8
+                address_zero_correction = 0
 
-        entry_size = 64
-        entry_cnt = (int.from_bytes(buf[0:field_size], "big") - 16 + address_zero_correction) // entry_size
+            else:
+                field_size = 4
+                address_zero_correction = -1
 
-        tmp = ""
-        for cnt, byte in enumerate(buf[0:field_size]):
-            tmp += hex(byte)
+            entry_cnt = (int.from_bytes(buf[0:field_size], "big") - 16 + address_zero_correction) // self.entry_size
 
-            if cnt != field_size - 1:
-                tmp += " "
+            for cnt in range(entry_cnt):
+                entry = buf[cnt * self.entry_size:((cnt + 1) * self.entry_size) - 1]
 
-        for cnt in range(entry_cnt):
-            entry = buf[cnt * entry_size:((cnt + 1) * entry_size) - 1]
+                """
+                Numbers inside zib files are stored in big endian. Presumably
+                the developer used a tool written in a language where that was
+                standard, maybe java
+                """
+                address = int.from_bytes(entry[0:field_size], "big")
 
-            """
-            Numbers inside zib files are stored in big endian. Presumably
-            the developer used a tool written in a language where that was
-            standard, maybe java
-            """
-            address = int.from_bytes(entry[0:field_size], "big")
+                if cnt == 0:
+                    address += address_zero_correction
+                    
+                size = int.from_bytes(entry[field_size:2 * field_size], "big")
 
-            if cnt == 0:
-                address += address_zero_correction
-            size = int.from_bytes(entry[field_size:2 * field_size], "big")
+                name = ""
+                for byte in entry[2 * field_size:64]:
+                    if byte == 0:
+                        break
 
-            name = ""
-            for byte in entry[2 * field_size:64]:
-                if byte == 0:
-                    break
-
-                try:
                     name += str(byte.to_bytes(1, "little").decode("utf-8"))
-                except ValueError:
-                    pass
-                    #set_trace()
 
-            self.entries.append(ZibEntry(address, size, name))
+                self.entries.append(ZibEntry(address, size, name))
 
+        else:
+            self.name = args[0]
+            self.entries = args[1]
+            final = self.entries[-1]
+            self.size = final.address + pad_size(final.size, 4)
+
+    def recompile(self, dest_handle, cursor):
+        if ".jpg" in self.name:
+            struct_string = ">QQ48s"
+            name_size = 48
+        else:
+            struct_string = ">LL56s"
+            name_size = 56
+
+        for cnt, entry in enumerate(self.entries):
+            path = join(self.name, entry.name)
+            dest_handle.seek(cursor + (self.entry_size * cnt))
+            dest_handle.write(pack(
+                            struct_string, 
+                            entry.address, 
+                            entry.size, 
+                            bytes(entry.name, "utf-8")))
+           
+            dest_handle.seek(cursor + entry.address)
+
+            with open(path, "rb") as src_handle:
+                dest_handle.write(src_handle.read())
+    
 class ZibEntry:
     def __init__(self, address, size, name):
         self.address = address
         self.size = size
         self.name = name
-
+        
 class Dat(File):
     def __init__(self, path, read = True):
         super().__init__(path, read)
         self.cursor = 0;
         self.word_size = 4
+
+        if not read:
+            with open(self.path, "wb") as handle:
+                pass
     
     def decompile_file(self, entry):
         with open(self.path, "rb") as src_handle:
@@ -110,8 +172,8 @@ class Dat(File):
         with open(self.path, "rb") as src_handle:
             src_handle.seek(self.cursor)
             self.zib = Zib(src_handle.read(entry.size), entry.name)
-
             dir_path = join(self.decompile_directory, self.zib.name)
+
             try:
                 mkdir(dir_path)
             except FileExistsError:
@@ -124,32 +186,97 @@ class Dat(File):
                 with open(path, "wb") as dest_handle:
                     dest_handle.write(src_handle.read(entry.size))
 
+    def recompile_file(self, path, dest_handle):
+        dest_handle.seek(self.cursor)
+
+        with open(path, "rb") as src_handle:
+            dest_handle.write(pad_bytes(src_handle.read(), 4))
+
+        self.increment_cursor(pad_size(getsize(path), 4))
+
+    def recompile_zib(self, entry, dest_handle):
+        entry.recompile(dest_handle, self.cursor)
+        self.increment_cursor(entry.size)
+
     """
     Ensure the cursor is always a multiple of the word size
     """
-    def increment_cursor(self, length):
-        self.cursor += length
-        overshoot = length % self.word_size
-
-        if overshoot != 0:
-            self.cursor += self.word_size - overshoot
+    def increment_cursor(self, size):
+        self.cursor += pad_size(size, self.word_size)
 
 class Order(File):
     def __init__(self, path, read = True):
         super().__init__(path, read)
+        self.entry_size = 64
+
+        if self.read:
+            self.entries = []
+
+            with open(self.path, "r") as handle:
+                lines = iter(handle.readlines())
+                for line in lines:
+                    line = line.strip()
+
+                    if ".zib" in line:
+                        word_size = 16
+                        zib_entries = []
+                        entry_cnt = int(next(lines))
+
+                        address = self.entry_size * entry_cnt
+                        address += 16
+
+                        for cnt in range(entry_cnt):
+                            name = next(lines).strip()
+                            size = getsize(join(line, name))
+                            zib_entries.append(ZibEntry(address, size, name))
+                            address += pad_size(size, word_size)
+
+                        self.entries.append(Zib(
+                                                line, 
+                                                zib_entries, 
+                                                read = False))
+
+                    else:
+                        self.entries.append(line)
     
     def add_entry(self, string):
         with open(self.path, "ab") as handle:
             handle.write(string.encode("utf-8"))
-            handle.write((0x000A).to_bytes(2, "big"))
+            handle.write((0x0A).to_bytes(1, "big"))
 
 class Files:
-    def __init__(self):
-        self.toc = Toc("reference/YGO_DATA.toc")
-        self.dat = Dat("reference/YGO_DATA.dat")
-        self.order = Order("reference/YGO_DAT.ord", read = False)
-        self.recompile_directory = "recompile"
+    def __init__(self, mode):
+        self.mode = mode
+        if self.mode == "decompile":
+            self.toc = Toc("reference/YGO_DATA.toc")
+            self.dat = Dat("reference/YGO_DATA.dat")
+            self.order = Order("reference/YGO_DAT.ord", read = False)
+        if self.mode == "recompile":
+            self.toc = Toc("recompile/YGO_DATA.toc", read = False)
+            self.dat = Dat("recompile/YGO_DATA.dat", read = False)
+            self.order = Order("reference/YGO_DAT.ord")
+        if (self.mode != "decompile") and (self.mode != "recompile"):
+            print(f"Error: Bad mode string passed to files.start({self.mode})")
+            raise ValueError
+
         self.decompile_directory = "decompile"
+        self.recompile_directory = "recompile"
+
+        try:
+            mkdir(self.decompile_directory)
+        except:
+            pass
+
+        try:
+            mkdir(self.recompile_directory)
+        except:
+            pass
+
+    def start(self):
+        if self.mode == "decompile":
+            self.decompile()
+        if self.mode == "recompile":
+            self.recompile()
 
     def decompile(self):
         for entry in self.toc.entries:
@@ -167,9 +294,23 @@ class Files:
 
             self.dat.increment_cursor(entry.size)
 
+    def recompile(self):
+        with open(self.dat.path, "wb") as handle:
+            for entry in self.order.entries:
+                if type(entry) == str:
+                    self.dat.recompile_file(entry, handle)
+                if type(entry) == Zib:
+                    self.dat.recompile_zib(entry, handle)
+
+                self.toc.add(entry)
+
 def main():
-    files = Files()
-    files.decompile()
+    try:
+        files = Files(argv[1])
+    except ValueError:
+        return -1
+
+    files.start()
 
     return 0
 
